00c05118a18b90b58bc141187ad99771c7154582
iuadget <iuadget@gmail.com>
2018-01-27 21:01:35 +0300

    update alien_invasion
    import classs Settings

--- src/main/python/alien_invasion.py
+++ src/main/python/alien_invasion.py
@@ -2,13 +2,15 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
 
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Airplane")
-
-    bg_color = (230, 230, 230)
 
     while True:
 
@@ -16,7 +18,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         pygame.display.flip()
 
