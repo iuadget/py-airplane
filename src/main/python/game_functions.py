3089b4ac532e7176d3333272f1e4bab06ea95fff
iuadget <iuadget@gmail.com>
2018-01-28 14:31:23 +0300

    add move up and down

--- src/main/python/game_functions.py
+++ src/main/python/game_functions.py
@@ -8,6 +8,10 @@ def check_keydown_events(event, ship):
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    if event.key == pygame.K_DOWN:
+        ship.moving_down = True
+    elif event.key == pygame.K_UP:
+        ship.moving_up = True
 
 def check_keyup_events(event, ship):
     """Реагируем на отпускания клавиш"""
@@ -15,6 +19,10 @@ def check_keyup_events(event, ship):
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
+    if event.key == pygame.K_DOWN:
+        ship.moving_down = False
+    elif event.key == pygame.K_UP:
+        ship.moving_up = False
 
 
 def check_events(ship):
--- src/main/python/ship.py
+++ src/main/python/ship.py
@@ -15,9 +15,13 @@ class Ship():
         self.rect.bottom = self.screen_rect.bottom
 
         self.center = float(self.rect.centerx)
+        self.bottom = float(self.rect.bottom)
 
         self.moving_right = False
         self.moving_left = False
+
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """"Обновляем позицию корабля с учетом флагов"""
@@ -26,7 +30,13 @@ class Ship():
         if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
 
+        if self.moving_down and self.rect.bottom < 800:
+            self.bottom += self.ai_settings.ship_speed_factor
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.bottom -= self.ai_settings.ship_speed_factor
+
         self.rect.centerx = self.center
+        self.rect.bottom = self.bottom
 
     def blitme(self):
 
